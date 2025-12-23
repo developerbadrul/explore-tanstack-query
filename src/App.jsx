78ad4d0879d8478bccfc3a29221e5ea26c94a66b
Md Badrul Alam <developer.badrul@gmail.com>
2025-12-23 19:08:06 +0600

    successfully add product with tanstack quary

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductDetails from './components/ProductDetails'
 import ProductList from './components/ProductList'
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
+import AddProduct from './components/AddProduct';
 
 const retrieveProducts = async ({ queryKey }) => {
   const response = await axios.get(`http://localhost:3000/${queryKey[0]}`);
@@ -19,13 +20,14 @@ function App() {
     queryFn: retrieveProducts
   })
 
-    console.log(products);
-    
+  // console.log(products);
+
   if (isLoading) return <div>Fetching Products...</div>
   if (error) return <div>An error occured: {error.message}</div>
 
   return (
     <div className="flex m-2">
+      <AddProduct />
       <ProductList products={products.data} onSelectProduct={setSelectedProduct} />
       {
         !selectedProduct ? "Product Not Selected" : <ProductDetails key={selectedProduct.id} id={selectedProduct.id} />
